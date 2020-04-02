c8396fd77ca905b7b9262a4569733e012b5dffde
attheodo <at@atworks.gr>
2020-04-02 17:39:32 +0300

    Initial Commit

---
+++ Pingu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pingu
+//
+//  Created by Thanos Theodoridis on 2/4/20.
+//  Copyright © 2020 Thanos Theodoridis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
