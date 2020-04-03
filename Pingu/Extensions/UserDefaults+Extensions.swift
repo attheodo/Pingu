0dad351cfe5a9563b9041bd073fe5e3ba197ca90
attheodo <at@atworks.gr>
2020-04-03 13:12:44 +0300

    Add key/value for storing preferred ping interval

---
+++ Pingu/Extensions/UserDefaults+Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaults+Extensions.swift
+//  Pingu
+//
+//  Created by Thanos Theodoridis on 2/4/20.
+//  Copyright © 2020 Thanos Theodoridis. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+    
+    var preferredPingInterval: TimeInterval {
+        
+        get {
+            return double(forKey: #function)
+        }
+        
+        set {
+            set(newValue, forKey: #function)
+            self.synchronize()
+        }
+        
+    }
+    
+}
+
+
