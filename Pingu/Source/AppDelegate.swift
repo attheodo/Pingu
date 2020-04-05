ef6fd1b590b9c68037efe080a40a465f9bc4eff0
attheodo <at@atworks.gr>
2020-04-05 15:41:51 +0300

    Kill PinguHelper if necessary

--- Pingu/Source/AppDelegate.swift
+++ Pingu/Source/AppDelegate.swift
@@ -18,11 +18,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // MARK: - Lifecycle
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
         pingu = Pingu()
+        killLaunchHelperIfNeccessary()
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    // MARK: - Private Methods
+    
+    fileprivate func killLaunchHelperIfNeccessary() {
+        
+        let runningApps = NSWorkspace.shared.runningApplications
+        let isRunning = !runningApps.filter { $0.bundleIdentifier == pinguLauncherBundleId }.isEmpty
+        
+        if isRunning {
+            DistributedNotificationCenter.default().post(name: .killPinguLauncher,
+                                                         object: Bundle.main.bundleIdentifier!)
+        }
+        
     }
 
 }
